59f8ceb3acdb39d57e496c9f18082c73656731b3
michaeldavidpryor <mdpcaps@gmail.com>
2020-10-31 20:40:18 -0400

    git ignore

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
-const Index = () => <h1>Notes</h1>;
+const Index = () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({ id: i, title: `Note: ${i}` }));
 
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map((note) => (
+        <div>
+          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Index;
